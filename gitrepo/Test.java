d2993a941b4b56d251b29b8cc89396f22efef802
Vishnu kale <kalevishnu759@gmail.com>
2024-02-04 23:25:56 +0530

    added

---
+++ gitrepo/Test.java
@@ -0,0 +1,25 @@
+package gitrepo;
+
+public class Test {
+    public static void main(String[] args) {
+        mulMethod();
+        addMethod();
+        
+    }
+    public static void addMethod(){
+        int k=330;
+        int l =320;
+        int add =k+l;
+        System.out.println(add);
+
+    }
+    public static void mulMethod(){
+        int m=30;
+        int n=20;
+        int mul =m*n;
+        System.out.println(mul);
+
+    }
+
+    
+}
